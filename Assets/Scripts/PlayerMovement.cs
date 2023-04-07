c256bd4160d603bc856f58215fd5cbeeda2160d2
Amit Verma <amit.verma.22080@gmail.com>
2023-04-07 15:18:34 +0530

    add fire delay

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Player Movement")]
     public float speedMultipler = 1f;
     public Transform player;
 
     // bullet
+    [Header("Bullet Properties")]
     public Transform bulletSpawnPos;
     public Transform bulletHolder;
     public GameObject bullet;
+
+    [Header("Shooting Properties")]
+    float delay = 0.3f;
+
+
+    // private
+    float time = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +30,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        time -= Time.deltaTime;
+
         float xPos = Input.GetAxisRaw("Horizontal");
         player.position += Vector3.right * speedMultipler * Time.deltaTime * xPos;
         
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0) && time <= 0) {
             shoot();
+            time = delay;
         }
     }
 
